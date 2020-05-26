41b567f5b50542b73dfe8367ffb6912f09a2be38
Rmonte Rodriguez <64934858+RmonteRodriguez@users.noreply.github.com>
2020-05-26 19:52:12 -0400

    Initial Commit

---
+++ PlayerOne.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerOne : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Reset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("MKey"))
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
